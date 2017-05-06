71b63d8f7b69b79c838849d46771bcf92bb6d82c
xiocis <xio_cis@mail.ru>
2017-05-06 19:58:35 +0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,20 +1,11 @@
-##var's
 num1=1
 num2=1
-q = True
-##while
-while (q==True):
-    print("Значение какого элемента ряда \
-Фибоначчи вы хотите узнать?")
-    n=int(input("Ввод - ")) 
-    i=2
-    if (n==1):
-        q=False
-    if (n!=1):
-        while (i<n):
-            fib=num2+num1
-            num1=num2
-            num2=fib
-            i+=1
-    print(fib)
-    print("Введите 1 для выхода из цикла")
+n=3
+i=2
+while(i<n) and (n<99999):
+    fib=num2 + num1
+    num1=num2
+    num2=fib
+    n+=1
+    i+=1
+print(fib, " ")

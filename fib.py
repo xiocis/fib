9e99a5078da658d04f1f7a813be4c8a9af1a7519
xiocis <xio_cis@mail.ru>
2017-05-06 20:10:01 +0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -2,10 +2,10 @@ num1=1
 num2=1
 n=3
 i=2
-while(i<n) and (n<99999):
-    fib=num2 + num1
+while(i<n):
+    fib=num2+num1
     num1=num2
     num2=fib
     n+=1
     i+=1
-print(fib, " ")
+    print(fib, " ")
